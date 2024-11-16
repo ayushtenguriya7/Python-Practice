08eed8282c07d7c8f5f79dcf4b7f5eda901baa84
AYUSH TENGURIYA <ayushtenguriya7@gmail.com>
2024-11-16 11:41:03 +0530

    Add files via upload
    Add loops in python file

---
+++ 5,1While_loop.py
@@ -0,0 +1,51 @@
+#LOOPS- Loops are used to repeat tasks.
+count=1
+while count<=5:
+    print("hello ayush")
+    count+=1
+
+#WAP to print numbers from 1to100
+i=1
+while i<=99:
+    print(i)
+    i+=1
+
+    #WAP to print numbers from 100to1
+u=100
+while u>=1:
+    print(u)
+    u-=1
+
+    #Print the multipliplication table of number n
+u=1
+while u<=10:
+    print(2*u)
+    u+=1
+
+#WAP in loop to print listnusing whilw loop
+
+list=[1,3,5,7,9]
+idx1=0
+while idx1<len(list):
+    print(list[idx1])
+    idx1+=1 
+
+    #search for a number x in the tuple using while loop
+
+tup=(1,3,9,5,6,0,2)
+x=2
+idx3=0
+while idx3<len(tup):
+    if (tup [idx3]==x):
+        print("found at idx",idx3)
+        idx3+=1
+
+
+#Break statement- it is used to stop the loop when encountered
+
+#continue-it terminates execution in current the current iteration & continues execution of the loop within next iteration.
+
+
+        
+        
+    

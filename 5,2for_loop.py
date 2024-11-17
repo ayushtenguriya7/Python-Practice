6393354ade69e6ed0df99a2937ab16c38a7d6e99
AYUSH TENGURIYA <ayushtenguriya7@gmail.com>
2024-11-17 11:13:41 +0530

    for loops in python

---
+++ 5,2for_loop.py
@@ -0,0 +1,13 @@
+#WAP to print all num from 1to 100 using for loop and range function
+for i in range(1,101,1):
+    print(i)
+    
+
+#WAP to print the table of n
+n=int(input("enter a num: "))
+for i in range(1,11):
+    print(n*i)
+
+#Range- range function returns a  sequence of numbers ,starting from 0 by default and increments by 1 by default ,and stops before a specified number
+#Pass- pass is a null statement that does nothing.it is used as placeholder for future code
+
